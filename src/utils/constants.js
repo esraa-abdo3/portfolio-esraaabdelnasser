cdf63b96b27dd2632fc427c89e592ef5af8ccb14
esraa-abdo3 <153438145+esraa-abdo3@users.noreply.github.com>
2024-09-27 17:50:57 +0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,82 +1,71 @@
-const projects = [
-  {
-      id: 1,
-    title: "to do list",
-    description:
-      "to do list.",
-    technologies: "html ,react,javascript , CSS",
-    src: "/images/projectsImages/todo.png",
-    demo: "https://to-do-list-iota-jet.vercel.app/",
-    code: "https://github.com/esraa-abdo3/to-do-list",
-  },
-    {
-      id: 2,
-    title: "my portfoio",
-    description:
-      "my portfoio.",
-    technologies: "html ,react,javascript,tailwind , CSS",
-    src: "/images/projectsImages/my.png",
-    demo: "https://portfolio-esraaabdelnasser-paqe.vercel.app/",
-    code: "https://github.com/esraa-abdo3/portfolio-esraaabdelnasser",
-  },
-  {
-    id: 3,
-    title: "Furniture",
-    description: "Furniture  store with React JS and  CSS",
-    technologies: "Javascript, CSS,",
-    src: "/images/projectsImages/furniturestorepage.png",
-    demo: "https://esraa-abdo3.github.io/furniture/",
-    code: "https://github.com/esraa-abdo3/furniture",
-  },
-    {
-    id: 4,
-    title: "Furniture",
-    description: "Furniture  store with React JS and  CSS",
-    technologies: "Javascript, CSS,",
-    src: "/images/projectsImages/furniturestorepage.png",
-    demo: "https://esraa-abdo3.github.io/furniture/",
-    code: "https://github.com/esraa-abdo3/furniture",
-  },
-  {
-    id: 5,
-    title: "learing-websites",
-    description:
-      "Landing page of learing-websites.",
-    technologies: " HTML5, Tailwind CSS",
-    src: "/images/projectsImages/learning.png",
-    demo: " https://esraa-abdo3.github.io/learning-website/",
-    code: "https://github.com/esraa-abdo3/learning-website",
-  },
-  {
-    id: 6,
-    title: "skincarepage",
-    description:
-      "Landing page of skincare-websites",
-    technologies: " HTML5, CSS3",
-    src: "/images/projectsImages/skincare.png",
-    demo: "https://esraa-abdo3.github.io/skin-care/",
-    code: "https://github.com/esraa-abdo3/skin-care",
-  },
-  {
-    id: 7,
-    title: "home store",
-    description:
-      "home store for shopping ",
-    technologies: " HTML5, CSS3",
-    src: "/images/projectsImages/home.png",
-    demo: " https://esraa-abdo3.github.io/homestore/",
-    code: "https://github.com/esraa-abdo3/homestore",
-  },
-  {
-    id: 8,
-    title: "cloth store",
-    description:
-      "cloth store.",
-    technologies: "html  , CSS",
-    src: "/images/projectsImages/cloth.png",
-    demo: "https://esraa-abdo3.github.io/clothstore/",
-    code: "https://github.com/esraa-abdo3/clothstore",
-  },
+const projects = [   
+  {     
+    id: 1,     
+    title: "to do list",     
+    description: "to do list.",     
+    technologies: "html, react, javascript, CSS",     
+    src: "/images/projectsImages/todo.png",     
+    demo: "https://to-do-list-iota-jet.vercel.app/",     
+    code: "https://github.com/esraa-abdo3/to-do-list",   
+  },   
+  {     
+    id: 2,     
+    title: "my portfolio",     
+    description: "my portfolio.",     
+    technologies: "html, react, javascript, tailwind, CSS",     
+    src: "/images/projectsImages/my.png",     
+    demo: "https://portfolio-esraaabdelnasser-paqe.vercel.app/",     
+    code: "https://github.com/esraa-abdo3/portfolio-esraaabdelnasser",   
+  },   
+  {     
+    id: 3,     
+    title: "Furniture",     
+    description: "Furniture store with React JS and CSS",     
+    technologies: "javascript, CSS",     
+    src: "/images/projectsImages/furniturestorepage.png",     
+    demo: "https://esraa-abdo3.github.io/furniture/",     
+    code: "https://github.com/esraa-abdo3/furniture",   
+  },   
+  {     
+    id: 4,     
+    title: "learing-websites",     
+    description: "Landing page of learing-websites.",     
+    technologies: "HTML5, Tailwind CSS",     
+    src: "/images/projectsImages/learning.png",     
+    demo: "https://esraa-abdo3.github.io/learning-website/",     
+    code: "https://github.com/esraa-abdo3/learning-website",   
+  },   
+  {     
+    id: 5,     
+    title: "skincarepage",     
+    description: "Landing page of skincare-websites.",     
+    technologies: "HTML5, CSS3",     
+    src: "/images/projectsImages/skincare.png",     
+    demo: "https://esraa-abdo3.github.io/skin-care/",     
+    code: "https://github.com/esraa-abdo3/skin-care",   
+  },   
+  {     
+    id: 6,     
+    title: "home store",     
+    description: "home store for shopping.",     
+    technologies: "HTML5, CSS3",     
+    src: "/images/projectsImages/home.png",     
+    demo: "https://esraa-abdo3.github.io/homestore/",     
+    code: "https://github.com/esraa-abdo3/homestore",   
+  },   
+  {     
+    id: 7,     
+    title: "cloth store",     
+    description: "cloth store.",     
+    technologies: "HTML, CSS",     
+    src: "/images/projectsImages/cloth.png",     
+    demo: "https://esraa-abdo3.github.io/clothstore/",     
+    code: "https://github.com/esraa-abdo3/clothstore",   
+  }
+];
+
+export default projects;
+
     
 
 
