749c7d85341f216df837c3c8323ecd784e5385a1
esraa-abdo3 <esraaabdalnasserzz@gmail.com>
2025-09-15 14:21:36 +0300

    fix: تعديل كذا في المشروع

--- src/components/pages/homepage-comps/CertificateSlider.jsx
+++ src/components/pages/homepage-comps/CertificateSlider.jsx
@@ -79,7 +79,7 @@ const CertificateSlider = ({ certificates }) => {
         <div className='space-y-4'>
         <div>
                 <h3 className='text-2xl font-semibold'>Faculty of Computers and Information</h3>
-                <p className='text-lg'>Current Student</p>
+                <p className='text-lg'>2021-2025</p>
             </div>
             <div>
                 <h3 className='text-2xl font-semibold'>Training at the Egyptian Company - Frontend Developer Track</h3>
